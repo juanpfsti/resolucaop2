46a823183d5ddf5c79e1dc698ac0ff42f0438711
Diego <diegoapf97@gmail.com>
2025-06-12 19:41:42 -0300

    criado a ListView, e o html todo_list listando as atividades, porém ainda não funciona os links pois serão colocados na proxima etapa da resolução.

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Todo
+
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
